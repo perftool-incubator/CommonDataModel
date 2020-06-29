72b86306c20c6221dbab43dd1eccdeb6d78829bb
Andrew Theurer <atheurer@redhat.com>
2020-06-29 13:43:33 -0400

    Update README and base files for ES 7.x

--- templates/build.sh
+++ templates/build.sh
@@ -6,28 +6,30 @@ index=$1
 file=$index.json
 echo '{' >$file
 echo '  "index_patterns": ["cdm'$ver'-'$index'*"],' >>$file
-echo '  "settings": {' >>$file
-echo '    "number_of_shards": 96,' >>$file
-echo '    "number_of_replicas": 2,' >>$file
-echo '    "codec" : "best_compression",' >>$file
-echo '    "refresh_interval" : "5s"' >>$file
-echo '  },' >>$file
-echo '  "mappings": {' >>$file
-echo '    "'$index'": {' >>$file
-echo '      "dynamic": "strict",' >>$file
-echo '      "properties": {' >>$file
+echo '  "template": {' >>$file
+echo '    "settings": {' >>$file
+echo '      "number_of_shards": 96,' >>$file
+echo '      "number_of_replicas": 2,' >>$file
+echo '      "codec" : "best_compression",' >>$file
+echo '      "refresh_interval" : "5s"' >>$file
+echo '    },' >>$file
+echo '    "mappings": {' >>$file
+echo '      "'$index'": {' >>$file
+echo '        "dynamic": "strict",' >>$file
+echo '        "properties": {' >>$file
 if [ "$index" != "metric_samp" ]; then
-	echo '        "cdm": {' >>$file
-	echo '          "properties": {' >>$file
-	echo '            "ver": { "type": "integer" },' >>$file
-	echo '            "doctype": { "type": "keyword" }' >>$file
-	echo '          }' >>$file
-	echo '        },' >>$file
+	echo '          "cdm": {' >>$file
+	echo '            "properties": {' >>$file
+	echo '              "ver": { "type": "integer" },' >>$file
+	echo '              "doctype": { "type": "keyword" }' >>$file
+	echo '            }' >>$file
+	echo '          },' >>$file
 fi
 while [ ! -z "$1" ]; do
     cat $1.base >> $file
     shift
 done
+echo "        }" >> $file
 echo "      }" >> $file
 echo "    }" >> $file
 echo "  }" >> $file
