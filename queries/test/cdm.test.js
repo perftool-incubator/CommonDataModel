9c469c3113f21df5fe94ce148b6c4d68202b0c98
mleader <mleader@redhat.com>
2023-10-10 12:14:23 -0400

    add jest cov report markdown action

--- queries/test/cdm.test.js
+++ queries/test/cdm.test.js
@@ -1,4 +1,5 @@
 const { describe, expect, test } = require('@jest/globals');
+
 const cdm = require('../cdmq/cdm.js');
 
 describe('set(ish)-array behavior', () => {
@@ -26,6 +27,35 @@ describe('set(ish)-array behavior', () => {
     expect(cdm.intersectAllArrays(a)).toStrictEqual([2]);
   });
 });
+
+// const { t } = require('tap');
+
+// t.test('set(ish) difference (subtractTwoArrays)', t => {
+//     a0 = [1, 1, 2];
+//     a1 = [2, 2, 3];
+
+//     t.matchOnlyStrict(cdm.subtractTwoArrays(a0, a1), [1, 1]);
+//     t.matchOnlyStrict(cdm.subtractTwoArrays(a1, a0), [3]);
+//     t.end();
+// });
+
+// t.test('set(ish) intersection (intersectTwoArrays)', t => {
+//     a0 = [1, 1, 2];
+//     a1 = [2, 2, 3];
+
+//     t.matchOnlyStrict(cdm.intersectTwoArrays(a0, a1), [2]);
+//     t.matchOnlyStrict(cdm.intersectTwoArrays(a1, a0), [2, 2]);
+//     t.end();
+// });
+
+// t.test('intersect all arrays', t => {
+//     a0 = [1, 1, 2];
+//     a1 = [2, 2, 3];
+//     a2 = [4, 4, 2];
+//     a = [a0, a1, a2];
+//     t.matchOnlyStrict(cdm.intersectAllArrays(a), [2]);
+//     t.end()
+// });
 
 
 
