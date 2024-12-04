8f511419f5f3dc7f1909c25a4f0d833ae1890893
Andrew Theurer <atheurer@redhat.com>
2024-12-04 06:42:11 -0500

    Fix header overflow for deletes
    - Adding wait_for_completion=false seems to avoid this issue
    - Added warning to user if all docs are not gone after all attempts
      at waiting

--- queries/cdmq/delete-run.js
+++ queries/cdmq/delete-run.js
@@ -9,27 +9,41 @@ program
 
 async function waitFor(docTypes) {
   var numAttempts = 1;
-  var maxAttempts = 10;
+  var maxAttempts = 30;
   var remainingDocTypes = docTypes;
+  var totalDocCount = 0;
+  var previousTotalDocCount = 0;
+  var interval = 5;
   while (numAttempts <= maxAttempts && docTypes.length > 0) {
     let promise = new Promise((resolve, reject) => {
-      setTimeout(() => resolve('done!'), 2000);
+      setTimeout(() => resolve('done!'), interval * 1000);
     });
     let result = await promise;
 
     console.log('\nConfirming all documents are in deleted OpenSearch (attempt #' + numAttempts + ')');
+    totalDocCount = 0;
     for (let i = 0; i < docTypes.length; i++) {
       var thisNumDocs = cdm.getDocCount(program.url, program.run, docTypes[i]);
       console.log('  ' + docTypes[i] + ': doc count: ' + thisNumDocs);
+      totalDocCount += thisNumDocs;
+
       if (thisNumDocs == 0) {
         remainingDocTypes = remainingDocTypes.filter((val) => val !== docTypes[i]);
       }
     }
     docTypes = remainingDocTypes;
     numAttempts++;
+
+    if (previousTotalDocCount != 0) {
+      console.log('Document deletion rate: %.2f documents/sec\n', (previousTotalDocCount - totalDocCount) / interval);
+    }
+    previousTotalDocCount = totalDocCount;
   }
   if (docTypes.lenth > 0) {
-    console.log('ERROR: could not delete all documents for ' + docTypes + ' with ' + numAttempts);
+    console.log('Warning: could not delete all documents for ' + docTypes + ' with ' + numAttempts);
+    console.log(
+      'These documents may continue to be deleted in the background.  To check on the status, run this utility again'
+    );
   }
 }
 
